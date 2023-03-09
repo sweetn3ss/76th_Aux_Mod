dda346541a1cf2583b36d2f8e75b8959316ae464
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2023-03-09 14:35:08 -0800

    initial core commit

---
+++ 76th_Core/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches
+{
+	class 76th_Core
+	{
+		name = "76th JTF Core";
+		author = "Mesa";
+		requiredVersion = 1.00;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+      	worlds[] = {};
+	};
+};
